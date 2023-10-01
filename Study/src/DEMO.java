fb9bc517f12bd779b91d57258f4ccdacdbda01e3
MDWIVEDI <MDWIVEDI@DVCI6123277.corp.amdocs.com>
2023-10-01 22:03:15 +0530

    Small change

--- Study/src/DEMO.java
+++ Study/src/DEMO.java
@@ -10,6 +10,7 @@ public class DEMO {
 		System.setProperty("webdriver.gecko.driver","C:\\Users\\MDWIVEDI\\Downloads\\geckodriver-v0.33.0-win64\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver(); //Creating an object of FirefoxDriver
 		driver.get("https://www.google.com/");
+		System.out.println("its Working");
 	}
 
 }
